936ad315d350a3e4a3444e4c4580209871d7b92f
Arnav Tiwari <tiwariarnav460@gmail.com>
2026-04-10 12:54:51 +0530

    second commit

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -1,8 +1,91 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import Quill from 'quill'
+import uniqid from 'uniqid'
+import { assets } from '../../assets/assets'
 
 const AddCourse = () => {
+
+  const quillRef = useRef(null)
+  const editorRef = useRef(null)
+
+  const[courseTitle, setCourseTitle] = useState('')
+  const[coursePrice, setCoursePrice] = useState(0)
+  const[discount, setDiscount] = useState()
+  const[image, setImage] = useState(null)
+  const[chapters, setChapters] = useState([])
+  const[showPopup, setShowPopup] = useState(false)
+  const[currentlyChapterId, setCurrentlyChapterId] = useState(null)
+  const[lectureDetails, setLectureDetails] = useState(
+    {
+      lectureTitle : '' , 
+      lectureDuration:'' , 
+      lectureUrl:'',
+      isPreviewFree : false ,
+    }
+  )
+
+  useEffect( () => {
+
+    if(!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme : 'snow' ,
+      });
+    }
+  } , [])
+
+
   return (
-    <div>AddCourse</div>
+    <div className='h-screen overflow-scroll flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0'>
+      <form className='flex flex-col gap-4 max-w-md w-full text-gray-500'> 
+        <div className='flex flex-col gap-1'> 
+          <p>course Title</p>
+          <input onChange={e => setCourseTitle(e.target.value)} value={courseTitle} type="text" 
+          placeholder='Type here' className='outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500'/>
+        </div>
+        <div className='flex flex-col gap-1'> 
+          <p>Course Decription</p>
+          <div ref={editorRef}></div>
+        </div>
+        <div className='flex items-center justify-between flex-wrap'>
+          <div className='flex flex-col gap-1'> 
+            <p>Course Price</p>
+            <input onChange={e => setCoursePrice(e.target.value)} value={coursePrice} type="text" 
+            placeholder='0' className='outline-none md:py-2.5 py-2 w-28 rounded border border-gray-500' required />
+          </div>
+
+          <div className='flex md:flex-row flex-col items-center gap-3'> 
+            <p>Course Tumbnail</p> 
+            <label htmlFor="thumbnailImage" className='p-3 bg-blue-500 rounded'>
+              <img src={assets.file_upload_icon} alt="" className='p-3 bg-blue-500 rounded'/>
+              <input type="file" id='thumbnailImage' onChange={e => setImage(e.target.files[0])} accept='image/*' hidden />
+              <img src={image ? URL.createObjectURL(image) : ''} alt="" />
+            </label>
+          </div>
+        </div>
+
+        <div> 
+          <p>Discount %</p>
+          <input onChange={e => setDiscount(e.target.value)} value={discount} type="number"
+          placeholder='0' max={100} className='outline-none md:py-2.5 py-2 w-28 px-3 rounded border border-gray-500' required />
+        </div>
+
+        <div> 
+          {chapters.map((chapter,index) => (
+            <div key={index} className='bg-white border rounded-lg mb-4'> 
+              <div className='flex justify-between items-center p-4 border-b'> 
+                <div className='flex items-center'>
+                <img src={assets.down_arrow_icon} width={14} alt="" className={`mr-2 cursor-pointer transition-all ${chapter.collapsed && "-rotate-90"}`} />
+                <span className='font-semibold'>{index + 1} {chapter.chapterTitle}</span>
+                </div>
+                <span className='text-gray-500'>{chapter.chapterContent.length} Lectures</span>
+              </div>
+            </div>
+          ))}
+        </div>
+
+      </form>
+
+    </div>
   )
 }
 
--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -24,7 +24,7 @@ const Dashboard = () => {
                   <div className='flex items-center gap-3 shadow-card border border-blue-500 p-4 w-56 rounded-md' >
                     ' <img src={assets.patients_icon}  />
                     <div> 
-                      <p className='text-2xl font-medium text-gray-600' >{dashboardData.enrolledStudentsData.lenght}</p>
+                      <p className='text-2xl font-medium text-gray-600' >{dashboardData.enrolledStudentsData.length}</p>
                       <p className='text-base text-gray-500'>Total Enrolments</p>
                     </div>
                   </div> 
@@ -44,6 +44,36 @@ const Dashboard = () => {
                       <p className='text-base text-gray-500'>Total Earning</p>
                     </div>
                   </div>
+              </div>
+              <div> 
+                <h2 className='pb-4 text-lg font-medium'>Latest Enrolments</h2>
+                <div className='flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md
+                bg-white border border-gray-500/20'>
+                  <table className='table-fixed md:text-auto w-full overflow-hidden'> 
+                    <thead className='text-gray-900 border-b border-gray-500/20 text-sm text-left'>
+                      <tr> 
+                        <th className='px-4 py-3 font-semibold text-center hidden sm:table-cell'>#</th>
+                        <th className='px-4 py-3 font-semibold'>Student Name</th>
+                        <th className='px-4 py-3 font-semibold'>Course Tittle</th>
+                      </tr>
+                    </thead>
+                    <tbody className='text-sm text-gray-500'> 
+                      {dashboardData.enrolledStudentsData.map( (item, index) => ( 
+                        <tr key={index} className='border-b border-gray-500/20'> 
+                          <td className='px-4 py-3 text-center hidden sm:table-cell'> 
+                              {index + 1}
+                          </td>
+                          <td className='md:px-4 px-2 py-3 flex items-center space-x-3'> 
+                            <img src={item.student.imageUrl} alt="profile" className='w-9 h-9 rounded-full' />
+                            <span className='truncate'>{item.student.name}</span>
+                          </td>
+                          <td>{item.courseTitle}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+
+                </div>
               </div>
           </div>
       </div>
